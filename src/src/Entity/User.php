2979255b7b72f8d84738ff0237cd25106e7be893
AFelipeTrujillo <andres.trujillo.1@gmail.com>
2024-07-10 15:02:11 +0200

    This commit includes the Security Controller and the Login Page

--- src/src/Entity/User.php
+++ src/src/Entity/User.php
@@ -131,5 +131,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getUserIdentifier(): string
     {
         // TODO: Implement getUserIdentifier() method.
+        return (string) $this->email;
     }
 }
